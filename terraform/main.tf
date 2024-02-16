04b1e279ff354f60758b8ea27a0a0198590a34c0
Esteban Alvarez Dávila <118398163+Igortiva@users.noreply.github.com>
2024-02-15 21:07:27 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "ec2_instance" {
   ami           = "ami-0c55b159cbfafe1f0"  # AMI de Amazon Linux 2
   instance_type = "t2.micro"
-  key_name      = "tu_nombre_de_clave_iam"  # Cambia esto por el nombre de tu clave IAM
+  key_name      = "PIN-ETAPA2"  # Cambia esto por el nombre de tu clave IAM
 
   tags = {
     Name = "MiInstanciaEC2"
